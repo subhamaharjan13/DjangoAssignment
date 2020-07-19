3200445c732fab9b70ea398d50b0ea2d9251f179
subhamaharjan <subhamaharjan13@gmail.com>
2020-07-19 16:59:08 +0545

    duplicate model

--- blogsite/blog/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-
-from .models import BlogInfo
-
-class BlogInfoModelForm(ModelForm):
-    class Meta:
-        model = BlogInfo
-        fields = ['blog_id','blog_title','blog_content','author_name']
